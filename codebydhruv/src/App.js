b8e2cdd5a63366e79da8fb398a749098bcb09e0b
DhruvIOS <shahdhruv684@gmail.com>
2026-03-22 20:59:44 -0400

    updated SEO

--- codebydhruv/src/App.js
+++ codebydhruv/src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import LandingPage from './Components/LadingPage';
 import AboutMe from './Components/AboutMe';
 import Navbar from './Components/Navbar';
-import Projects from './Components/Projects'
+import Projects from './Components/Projects';
+import { Helmet } from 'react-helmet-async';
 
 function App() {
+  const canonicalUrl = `https://www.codebydhruv.dev${window.location.pathname}`;
+
   return (
     <div className="App">
-
+      <Helmet>
+        <link rel="canonical" href={canonicalUrl} />
+      </Helmet>
       <Navbar />
       <LandingPage />
       <AboutMe />
